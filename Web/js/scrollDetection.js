cb273b8b06676e16ab7c4768a0f5d8e15c370d73
Heilmann Jonathan <jonathan005h@gmail.com>
2022-05-27 16:03:19 +0200

    Espacement entre section

--- Web/js/scrollDetection.js
+++ Web/js/scrollDetection.js
@@ -24,7 +24,7 @@ function Anim(name) {
 function checkVisible(elm) {
     var rect = elm.getBoundingClientRect();
     var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= -200);
 }
 
 //Regarde si une div est affichée toutes les 250ms puis désactive l'interval
